4e9598ba2f695fd7d6ddbbc554a9471ab1653fc9
ETelyatnikova <ETelyatnikova@users.noreply.github.com>
2021-08-14 23:39:54 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,9 +6,11 @@ def buildApp() {
 
 def buildImage() {
   echo 'building the docker image...'
+  // prepare docker build context
+  sh "cp target/project.war ./tmp-docker-build-context"
   withDockerServer([uri:'tcp://docker:2376']){
     withDockerRegistry([credentialsId: 'nexus-docker-repo', url: 'https://192.168.1.77:8083']) { 
-      def myImage = docker.build("192.168.1.77:8083/demo-app:${params.VERSION}")
+      def myImage = docker.build("192.168.1.77:8083/demo-app:${params.VERSION}", "--build-arg PACKAGE_VERSION=${params.VERSION} ./tmp-docker-build-context")
       myImage.push()
     }
   }
