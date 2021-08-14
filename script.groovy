022c72f5b8f6b7d8504508bc4c4f8c69ec64f311
ETelyatnikova <ETelyatnikova@users.noreply.github.com>
2021-08-14 14:07:44 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,5 +1,15 @@
 def buildApp() {
   echo 'building the app...'
+  sh 'npm install'
+  sh 'npm run build' 
+}
+
+def buildImage() {
+  echo 'building the docker image...'
+  withCredintials([usernamePassword(credentialsId: 'nexus-docker-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){
+    sh "docker build -t 192.168.1.77:8083/demo-app:${params.VERSION}"
+    sh "echo $PASS | docker login -u $USER --password-stdin 192.168.1.77:8083"
+    sh "docker push 192.168.1.77:8083/demo-app:${params.VERSION}"
 }
 
 def testApp() {
